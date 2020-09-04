1149bfc13ed09253555f7bf075ce8b591262e5c5
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2020-09-04 13:10:37 +0800

    section 3-49

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,41 +1,120 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+//import React, { useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
+/*
+const app = (props) => {
+  //useState returns an array with exactly 2 elements
+  //first element - returns current state
+  //second element - function used to update state
+  //const stateArr = useState({
+  const [personState, setPersonsState] = useState({
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Jake", age: 37 },
+    ],
+    otherState: "some other value",
+  });
+  //if state changes it will make react re-render dom
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: "Blake", age: 24 },
+      ],
+      otherState: personState.otherState,
+    });
+  };
+
+  //hooks dont have render function
+  return (
+    <div className="App">
+      <h1> Hi I'm a react developer</h1>
+      <p>This is really working !</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      />
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      />
+      <Person name="Max" age="28" />
+      <Person name="Manu" age="29" />
+      My Hobbies: Racing
+      <Person />
+    </div>
+  );
+};
+*/
 
 class App extends Component {
-
   //if state changes it will make react re-render dom
-  state ={
+  state = {
     persons: [
-      { name: 'Max', age:28 },
-      { name: 'Jake', age:37 },
-    ]
-  }
+      { name: "Max", age: 28 },
+      { name: "Jake", age: 37 },
+    ],
+  };
 
   //Handler is used to define a method that were not actively calling but assigning as an event handler
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //Dont do this to change state: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Max', age:28 },
-        { name: 'Blake', age:24 },
-      ]
-    }) //allows us to update the states
+        { name: newName, age: 28 },
+        { name: "Blake", age: 24 },
+      ],
+    }); //allows us to update the states
     //console.log('Was clicked');
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+        persons: [
+          { name: "Manu", age: 42 },
+          { name: event.target.value, age: 49}
+        ]
+
+    })
+
   }
+
+
+
+
 
 
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    };
+
+
+
     return (
       <div className="App">
         <h1> Hi I'm a react developer</h1>
         <p>This is really working !</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name ={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name = "Max" age ="28"/>
-        <Person name = "Manu" age="29"/>My Hobbies: Racing<Person />
+        <button
+          style={style} 
+          onClick={() => this.switchNameHandler("Maximilian!")}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed = {this.nameChangedHandler}
+          click= {this.switchNameHandler.bind(this,"Max")}> My Hobbies: Racing </Person>
 
       </div>
     );
@@ -43,3 +122,4 @@ class App extends Component {
 }
 
 export default App;
+//export default app;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm  {props.name}  and i am {props.age} years old</p>
+        <div className = "Person">
+            <p onClick={props.click}>  I'm  {props.name}  and i am {props.age} years old</p>
+            <input type='text' onChange={props.changed} />
         </div>
     )
 };
